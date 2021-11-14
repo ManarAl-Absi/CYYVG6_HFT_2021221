cbe862c5c5411790935302f41e3c9b53958c3e39
ManarAl-Absi <manar739231959@gmail.com>
2021-11-14 18:30:11 +0100

    Fill the StudentRepository with 5 public methods

--- CYYVG6_HFT_2021221.Repository/StudentRepository.cs
+++ CYYVG6_HFT_2021221.Repository/StudentRepository.cs
@@ -8,26 +8,40 @@ using System.Threading.Tasks;
 
 namespace CYYVG6_HFT_2021221.Repository
 {
-    class StudentRepository : Repository<Student>, IStudentRepository
+    public class StudentRepository : Repository<Student>, IStudentRepository
     {
         public StudentRepository(DbContext DbCntx)
             : base(DbCntx)
         {
         }
 
+        public void ChangeMajorWithinTheFaculty(int id, string newMajor)
+        {
+            var student = this.GetOne(id);
+            if (student == null)
+            {
+                throw new InvalidOperationException("Sorry! This student is not in our university");
+            }
+            student.Major = newMajor;
+            this.Context.SaveChanges();
+        }
+
         public override Student GetOne(int id)
         {
-            throw new NotImplementedException();
+            return this.GetAll().SingleOrDefault(x => x.StudentId == id);
         }
 
         public override void Insert(Student entity)
         {
-            throw new NotImplementedException();
+            this.Context.Set<Student>().Add(entity);
+            this.Context.SaveChanges();
         }
 
         public override void Remove(int id)
         {
-            throw new NotImplementedException();
+            Student stud = this.GetOne(id);
+            this.Context.Set<Student>().Remove(stud);
+            this.Context.SaveChanges();
         }
     }
 }
