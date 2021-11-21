312dbc8a2552888f1544b2d03f345f11e94de4ee
ManarAl-Absi <manar739231959@gmail.com>
2021-11-21 23:47:45 +0100

    Create 1 test class and 5 void methods

--- CYYVG6_HFT_2021221.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CYYVG6_HFT_2021221.Test
-{
-    public class Class1
-    {
-    }
-}
---
+++ CYYVG6_HFT_2021221.Test/UniversityLogicTest.cs
@@ -0,0 +1,48 @@
+﻿using CYYVG6_HFT_2021221.Repository;
+using CYYVG6_HFT_2021221.Logic;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace CYYVG6_HFT_2021221.Test
+{
+    [TestFixture]
+    public class UniversityLogicTest
+    {
+        private Mock<IStudentRepository> studentRepository;
+        private Mock<IEmployeeRepository> employeeRepository;
+        private Mock<IFacultyRepository> facultyRepository;
+        //private UniversityLogic universityLogic;
+
+        [Test]
+        public void TestStudentCosts()
+        {
+          
+        }
+
+        [Test]
+        public void TestStudentMajor()
+        {
+
+        }
+
+
+        [Test]
+        public void TestEmployeeEarnings()
+        {
+        }
+
+        
+        [Test]
+        public void TestEmployeePosition()
+        {
+        }
+
+        
+        [OneTimeSetUp]
+        public void CreateLogicWithMocks()
+        {
+        }
+
+        }
+}
