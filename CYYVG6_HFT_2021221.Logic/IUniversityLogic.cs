c4748603f0c1e40c3035a87b911470daf1ea9930
ManarAl-Absi <manar739231959@gmail.com>
2021-11-21 21:58:38 +0100

    Fill an interface

---
+++ CYYVG6_HFT_2021221.Logic/IUniversityLogic.cs
@@ -0,0 +1,31 @@
+﻿using CYYVG6_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYYVG6_HFT_2021221.Logic
+{
+    interface IUniversityLogic
+    {
+        Faculty GetOneFaculty(int id);
+        IList<Faculty> GetAllFaculties();
+
+        Student GetOneStudent(int id);
+        IList<Student> GetAllStudents();
+
+        Employee GetOneEmployee(int id);
+        IList<Employee> GetAllEmployees();
+
+        void ChangeEmployeeSalary(int id, int newSalary);
+        void ChangeEmployeePosition(int id, int newPosition);
+        void changeStudentFaculty(int id, Faculty newFaculty);
+        void changeFacultyAddress(int id, string newAdress);
+        void ChangeEmployeeFaculty(int id, Faculty newFaculty);
+
+        IList<TotalEmployeeSalary> EmployeePayment();
+
+
+    }
+}
