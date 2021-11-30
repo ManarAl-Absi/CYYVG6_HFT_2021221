1c631bfba2f01878c02e7a7faeeb1f38252916cf
ManarAl-Absi <manar739231959@gmail.com>
2021-11-30 23:25:08 +0100

    Add the program of the Client

--- CYYVG6_HFT_2021221.Client/Program.cs
+++ CYYVG6_HFT_2021221.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CYYVG6_HFT_2021221.Models;
+using System;
 
 namespace CYYVG6_HFT_2021221.Client
 {
@@ -6,7 +7,32 @@ namespace CYYVG6_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(8000);
+
+            RestService rest = new RestService("http://localhost:54726");
+
+
+            rest.Post<Faculty>(new Faculty()
+            {
+                FacultyName = "Dob Faculty for Art",
+                FacultyAddress = "1034 Budapest, Dob u. 9"
+
+            }, "faculty");
+
+            var faculties = rest.Get<Faculty>("faculty");
+            var students = rest.Get<Student>("student");
+            var employees = rest.Get<Employee>("employees");
+
+
+
+            int highestsalary = rest.GetSingle<int>("stat/highestsalary");
+            int salaryuniversitypayforallemp = rest.GetSingle<int>("stat/salaryuniversitypayforallemp");
+            double avgageofstudents = rest.GetSingle<double>("stat/avgageofstudents");
+            int numofstudeniIuUniversity = rest.GetSingle<int>("stat/numofstudeniIuUniversity");
+            int moneyuniversityearnfromstudent = rest.GetSingle<int>("stat/moneyuniversityearnfromstudent");
+
+
+
         }
     }
 }
